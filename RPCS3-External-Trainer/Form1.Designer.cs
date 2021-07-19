2a89ce1cfc77252dad033efd79acb4237550b622
xCENTx <80198020+xCENTx@users.noreply.github.com>
2021-07-19 01:15:50 -0400

    New Methods
    - AoB Scan
    - Patch Bytes
    - UI Changes

--- RPCS3-External-Trainer/Form1.Designer.cs
+++ RPCS3-External-Trainer/Form1.Designer.cs
@@ -35,12 +35,27 @@ namespace RPCS3_External_Trainer_SOURCE
             this.RPCS3Process_Timer = new System.Windows.Forms.Timer(this.components);
             this.RPCS3Mem_Timer = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.Souls_Button = new System.Windows.Forms.Button();
             this.SendSouls_textBox = new System.Windows.Forms.TextBox();
+            this.NewMoonGrass_textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CurrentNewMoonGrass_Label = new System.Windows.Forms.Label();
+            this.PatternScan_Button = new System.Windows.Forms.Button();
+            this.PatternScan2_Button = new System.Windows.Forms.Button();
+            this.SoldiersSouls_textBox = new System.Windows.Forms.TextBox();
+            this.SoldiersSouls_Button = new System.Windows.Forms.Button();
+            this.CurrentSoldiersSouls_Label = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.InfiniteHealth_Button = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // rpcs3Status
@@ -58,10 +73,9 @@ namespace RPCS3_External_Trainer_SOURCE
             // CurrentSouls_Label
             // 
             this.CurrentSouls_Label.AutoSize = true;
-            this.CurrentSouls_Label.Dock = System.Windows.Forms.DockStyle.Top;
             this.CurrentSouls_Label.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CurrentSouls_Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.CurrentSouls_Label.Location = new System.Drawing.Point(0, 0);
+            this.CurrentSouls_Label.Location = new System.Drawing.Point(1, 2);
             this.CurrentSouls_Label.Name = "CurrentSouls_Label";
             this.CurrentSouls_Label.Size = new System.Drawing.Size(198, 24);
             this.CurrentSouls_Label.TabIndex = 4;
@@ -80,14 +94,24 @@ namespace RPCS3_External_Trainer_SOURCE
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(27)))), ((int)(((byte)(25)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.rpcs3Status);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 70);
+            this.panel1.Location = new System.Drawing.Point(0, 243);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(274, 26);
             this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(190, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(83, 26);
+            this.panel2.TabIndex = 7;
             // 
             // label1
             // 
@@ -95,50 +119,165 @@ namespace RPCS3_External_Trainer_SOURCE
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(-2, 5);
+            this.label1.Location = new System.Drawing.Point(-2, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 14);
             this.label1.TabIndex = 6;
             this.label1.Text = "THIS IS A TEST";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel2
-            // 
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(192, 70);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(83, 50);
-            this.panel2.TabIndex = 7;
-            // 
             // Souls_Button
             // 
-            this.Souls_Button.Location = new System.Drawing.Point(117, 27);
+            this.Souls_Button.Location = new System.Drawing.Point(151, 27);
             this.Souls_Button.Name = "Souls_Button";
-            this.Souls_Button.Size = new System.Drawing.Size(124, 23);
+            this.Souls_Button.Size = new System.Drawing.Size(65, 23);
             this.Souls_Button.TabIndex = 8;
-            this.Souls_Button.Text = "SEND SOULS";
+            this.Souls_Button.Text = "SEND";
             this.Souls_Button.UseVisualStyleBackColor = true;
             this.Souls_Button.Click += new System.EventHandler(this.Souls_Button_Click);
             // 
             // SendSouls_textBox
             // 
-            this.SendSouls_textBox.Location = new System.Drawing.Point(11, 29);
+            this.SendSouls_textBox.Location = new System.Drawing.Point(68, 29);
             this.SendSouls_textBox.Name = "SendSouls_textBox";
-            this.SendSouls_textBox.Size = new System.Drawing.Size(100, 20);
+            this.SendSouls_textBox.Size = new System.Drawing.Size(77, 20);
             this.SendSouls_textBox.TabIndex = 9;
+            // 
+            // NewMoonGrass_textBox
+            // 
+            this.NewMoonGrass_textBox.Location = new System.Drawing.Point(68, 31);
+            this.NewMoonGrass_textBox.Name = "NewMoonGrass_textBox";
+            this.NewMoonGrass_textBox.Size = new System.Drawing.Size(77, 20);
+            this.NewMoonGrass_textBox.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DarkMoonGrass_Button_Click);
+            // 
+            // CurrentNewMoonGrass_Label
+            // 
+            this.CurrentNewMoonGrass_Label.AutoSize = true;
+            this.CurrentNewMoonGrass_Label.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentNewMoonGrass_Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CurrentNewMoonGrass_Label.Location = new System.Drawing.Point(1, 2);
+            this.CurrentNewMoonGrass_Label.Name = "CurrentNewMoonGrass_Label";
+            this.CurrentNewMoonGrass_Label.Size = new System.Drawing.Size(243, 24);
+            this.CurrentNewMoonGrass_Label.TabIndex = 10;
+            this.CurrentNewMoonGrass_Label.Text = "CurrentNewMoonGrass";
+            // 
+            // PatternScan_Button
+            // 
+            this.PatternScan_Button.Location = new System.Drawing.Point(6, 183);
+            this.PatternScan_Button.Name = "PatternScan_Button";
+            this.PatternScan_Button.Size = new System.Drawing.Size(130, 23);
+            this.PatternScan_Button.TabIndex = 13;
+            this.PatternScan_Button.Text = "NEVER LOSE SOULS";
+            this.PatternScan_Button.UseVisualStyleBackColor = true;
+            this.PatternScan_Button.Click += new System.EventHandler(this.PatternScan_Button_Click);
+            // 
+            // PatternScan2_Button
+            // 
+            this.PatternScan2_Button.Location = new System.Drawing.Point(92, 212);
+            this.PatternScan2_Button.Name = "PatternScan2_Button";
+            this.PatternScan2_Button.Size = new System.Drawing.Size(99, 23);
+            this.PatternScan2_Button.TabIndex = 14;
+            this.PatternScan2_Button.Text = "D3BUG";
+            this.PatternScan2_Button.UseVisualStyleBackColor = true;
+            this.PatternScan2_Button.Click += new System.EventHandler(this.PatternScan2_Button_Click);
+            // 
+            // SoldiersSouls_textBox
+            // 
+            this.SoldiersSouls_textBox.Location = new System.Drawing.Point(68, 31);
+            this.SoldiersSouls_textBox.Name = "SoldiersSouls_textBox";
+            this.SoldiersSouls_textBox.Size = new System.Drawing.Size(77, 20);
+            this.SoldiersSouls_textBox.TabIndex = 17;
+            // 
+            // SoldiersSouls_Button
+            // 
+            this.SoldiersSouls_Button.Location = new System.Drawing.Point(151, 29);
+            this.SoldiersSouls_Button.Name = "SoldiersSouls_Button";
+            this.SoldiersSouls_Button.Size = new System.Drawing.Size(65, 23);
+            this.SoldiersSouls_Button.TabIndex = 16;
+            this.SoldiersSouls_Button.Text = "SEND";
+            this.SoldiersSouls_Button.UseVisualStyleBackColor = true;
+            this.SoldiersSouls_Button.Click += new System.EventHandler(this.SoldiersSouls_Button_Click);
+            // 
+            // CurrentSoldiersSouls_Label
+            // 
+            this.CurrentSoldiersSouls_Label.AutoSize = true;
+            this.CurrentSoldiersSouls_Label.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentSoldiersSouls_Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CurrentSoldiersSouls_Label.Location = new System.Drawing.Point(1, 2);
+            this.CurrentSoldiersSouls_Label.Name = "CurrentSoldiersSouls_Label";
+            this.CurrentSoldiersSouls_Label.Size = new System.Drawing.Size(154, 24);
+            this.CurrentSoldiersSouls_Label.TabIndex = 15;
+            this.CurrentSoldiersSouls_Label.Text = "Soldier\'sSouls";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.CurrentSouls_Label);
+            this.panel3.Controls.Add(this.Souls_Button);
+            this.panel3.Controls.Add(this.SendSouls_textBox);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(274, 59);
+            this.panel3.TabIndex = 18;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.CurrentNewMoonGrass_Label);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.NewMoonGrass_textBox);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 59);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(274, 59);
+            this.panel4.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.CurrentSoldiersSouls_Label);
+            this.panel5.Controls.Add(this.SoldiersSouls_Button);
+            this.panel5.Controls.Add(this.SoldiersSouls_textBox);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 118);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(274, 59);
+            this.panel5.TabIndex = 20;
+            // 
+            // InfiniteHealth_Button
+            // 
+            this.InfiniteHealth_Button.Location = new System.Drawing.Point(146, 183);
+            this.InfiniteHealth_Button.Name = "InfiniteHealth_Button";
+            this.InfiniteHealth_Button.Size = new System.Drawing.Size(120, 23);
+            this.InfiniteHealth_Button.TabIndex = 21;
+            this.InfiniteHealth_Button.Text = "INFINITE HEALTH";
+            this.InfiniteHealth_Button.UseVisualStyleBackColor = true;
+            this.InfiniteHealth_Button.Click += new System.EventHandler(this.InfiniteHealth_Button_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
-            this.ClientSize = new System.Drawing.Size(274, 96);
-            this.Controls.Add(this.SendSouls_textBox);
-            this.Controls.Add(this.Souls_Button);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(274, 269);
+            this.Controls.Add(this.InfiniteHealth_Button);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.PatternScan2_Button);
+            this.Controls.Add(this.PatternScan_Button);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.CurrentSouls_Label);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.ShowIcon = false;
@@ -148,8 +287,13 @@ namespace RPCS3_External_Trainer_SOURCE
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -163,6 +307,18 @@ namespace RPCS3_External_Trainer_SOURCE
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button Souls_Button;
         private System.Windows.Forms.TextBox SendSouls_textBox;
+        private System.Windows.Forms.TextBox NewMoonGrass_textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label CurrentNewMoonGrass_Label;
+        private System.Windows.Forms.Button PatternScan_Button;
+        private System.Windows.Forms.Button PatternScan2_Button;
+        private System.Windows.Forms.TextBox SoldiersSouls_textBox;
+        private System.Windows.Forms.Button SoldiersSouls_Button;
+        private System.Windows.Forms.Label CurrentSoldiersSouls_Label;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button InfiniteHealth_Button;
     }
 }
 
--- RPCS3-External-Trainer/Form1.cs
+++ RPCS3-External-Trainer/Form1.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 using System.Globalization;
 using System.Numerics;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
 
 namespace RPCS3_External_Trainer_SOURCE
 {
@@ -21,6 +24,72 @@ namespace RPCS3_External_Trainer_SOURCE
                    ((x & 0xff000000) >> 24);
         }
 
+        #region AoB Scans
+
+        public async void PatternScanSoulsDecrementFunction()
+        {
+            if (!m.OpenProcess(RPCS3PROCESSNAME))
+            {
+                MessageBox.Show("Process Is Not Found or Open!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //AoB Scan & Store Result
+            IEnumerable<long> AoBScanResults = await m.AoBScan("4C 89 75 18 48 8B 4D 58 89 44 19 78", false, true);
+            long SingleAoBScanResult = AoBScanResults.FirstOrDefault();
+            var FinalResult = SingleAoBScanResult + 0x08;
+
+            // Write 
+            uint intValue = 0x90909090;
+            byte[] intBytes = BitConverter.GetBytes(intValue);
+            Array.Reverse(intBytes);
+            m.WriteBytes(FinalResult.ToString("X"), intBytes);
+        }
+
+        public async void PatternScanSoulsLossOnDeath()
+        {
+            if (!m.OpenProcess(RPCS3PROCESSNAME))
+            {
+                MessageBox.Show("Process Is Not Found or Open!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //AoB Scan & Store Result
+            IEnumerable<long> AoBScanResults = await m.AoBScan("4C 89 75 18 48 8B 4D 50 89 44 19 78", false, true);
+            long SingleAoBScanResult = AoBScanResults.FirstOrDefault();
+            var FinalResult = SingleAoBScanResult + 0x8;
+
+            // Write 
+            uint intValue = 0x90909090;
+            byte[] intBytes = BitConverter.GetBytes(intValue);
+            Array.Reverse(intBytes);
+            m.WriteBytes(FinalResult.ToString("X"), intBytes);
+        }
+
+        public async void InfiniteHealthHack()
+        {
+            if (!m.OpenProcess(RPCS3PROCESSNAME))
+            {
+                MessageBox.Show("Process Is Not Found or Open!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //AoB Scan & Store Result
+            IEnumerable<long> AoBScanResults = await m.AoBScan("F0 00 00 00 89 84 19 C4 03 00 00 7E 09 48 83 C4 28", false, true);
+            long SingleAoBScanResult = AoBScanResults.FirstOrDefault();
+            var FinalResult = SingleAoBScanResult + 0x3;
+            var FinalResult2 = FinalResult + 0x4;
+            
+            // Write 
+            uint intValue = 0x00909090;
+            uint intValue2 = 0x90909090;
+            byte[] intBytes = BitConverter.GetBytes(intValue);
+            byte[] intBytes2 = BitConverter.GetBytes(intValue2);
+            Array.Reverse(intBytes2);
+            Array.Reverse(intBytes);
+            m.WriteBytes(FinalResult.ToString("X"), intBytes);
+            m.WriteBytes(FinalResult2.ToString("X"), intBytes2);
+        }
+
+        #endregion
+
         public MainForm()
         {
             InitializeComponent();
@@ -35,6 +104,8 @@ namespace RPCS3_External_Trainer_SOURCE
             }
         }
 
+        #region TIMERS
+
         private void RPCS3Process_Timer_Tick(object sender, EventArgs e)
         {
             int PID = m.GetProcIdFromName(RPCS3PROCESSNAME);
@@ -46,7 +117,10 @@ namespace RPCS3_External_Trainer_SOURCE
                 rpcs3Status.ForeColor = Color.FromArgb(0, 169, 0);
                 rpcs3Status.BackColor = Color.FromArgb(32, 32, 35);
                 rpcs3Status.Text = "RPCS3 CONNECTED";
+
+                // This nifty bool is pretty helpful :)
                 rpcs3Running = true;
+                
                 return;
             }
             rpcs3Status.Text = "RPCS3 NOT DETECTED";
@@ -57,31 +131,123 @@ namespace RPCS3_External_Trainer_SOURCE
 
         private void RPCS3Mem_Timer_Tick(object sender, EventArgs e)
         {
+            // aye if it aint running , we aint wasting the resources mmkay?
             if (!rpcs3Running)
             {
                 return;
             }
+
             //Reads Current Souls and displays the value via Label
             BigInteger parseSoulsAddr = BigInteger.Parse("3301E8098", NumberStyles.HexNumber);
             var SoulsAddr = parseSoulsAddr.ToString("X");
             var SoulsAddrValue = m.ReadUInt(SoulsAddr);
             var CURRENT_SOULS = SwapBytes(SoulsAddrValue);
-            CurrentSouls_Label.Text = "CURRENT SOULS: " + Convert.ToString(CURRENT_SOULS);
+            CurrentSouls_Label.Text = "Current Souls: " + Convert.ToString(CURRENT_SOULS);
+
+            //Reads Current NewMoonGrass and displays the value via label
+            BigInteger parseNewMoonGrass = BigInteger.Parse("3301E89E0", NumberStyles.HexNumber);
+            var NewMoonGrassAddr = parseNewMoonGrass.ToString("X");
+            var NewMoonGrassValue = m.ReadUInt(NewMoonGrassAddr);
+            var CURRENT_NEWMOONGRASS = SwapBytes(NewMoonGrassValue);
+            CurrentNewMoonGrass_Label.Text = "New Moon Grass: " + Convert.ToString(CURRENT_NEWMOONGRASS);
+
+            //Reads Current SoldiersSouls and displays the value via Label
+            BigInteger parseSoldiersSouls = BigInteger.Parse("3301E8700", NumberStyles.HexNumber);
+            var SoldiersSoulsAddr = parseSoldiersSouls.ToString("X");
+            var SoldiersSoulsValue = m.ReadUInt(SoldiersSoulsAddr);
+            var CURRENT_SOLDIERSSOULS = SwapBytes(SoldiersSoulsValue);
+            CurrentSoldiersSouls_Label.Text = "Current Soldiers Souls: " + Convert.ToString(CURRENT_SOLDIERSSOULS);
+
         }
 
+        #endregion
+
+        #region BUTTONS
+        // Need to provide more paremeters for the buttons , for instance if there is text in the textbox etc
         private void Souls_Button_Click(object sender, EventArgs e)
         {
             if (!rpcs3Running)
             {
                 return;
             }
+            // Player Souls Address
             BigInteger parseSoulsAddr = BigInteger.Parse("3301E8098", NumberStyles.HexNumber);
             string hex = $"0x{parseSoulsAddr:X8}";
+            
+            // Convert Textbox text to to int
             int intValue = Convert.ToInt32(SendSouls_textBox.Text);
+            
+            // Convert & Reverse bytes
             byte[] intBytes = BitConverter.GetBytes(intValue);
             Array.Reverse(intBytes);
-            byte[] result = intBytes;
+            
+            // Write Value in textbox to memory
             m.WriteBytes(hex, intBytes);
         }
+
+        private void DarkMoonGrass_Button_Click(object sender, EventArgs e)
+        {
+            if (!rpcs3Running)
+            {
+                return;
+            }
+            // Address
+            BigInteger parseDarkMoonGrassAddr = BigInteger.Parse("3301E89E0", NumberStyles.HexNumber);
+            string hex = $"0x{parseDarkMoonGrassAddr:X8}";
+            
+            // Convert textbox text to int
+            int intValue = Convert.ToInt32(NewMoonGrass_textBox.Text);
+            
+            // Convert & Reverse bytes
+            byte[] intBytes = BitConverter.GetBytes(intValue);
+            Array.Reverse(intBytes);
+            
+            // Write Value in textbox to memory
+            m.WriteBytes(hex, intBytes);
+
+        }
+
+        private void SoldiersSouls_Button_Click(object sender, EventArgs e)
+        {
+            if (!rpcs3Running)
+            {
+                return;
+            }
+            // Address
+            BigInteger parseSoldiersSoulsAddr = BigInteger.Parse("3301E8700", NumberStyles.HexNumber);
+            string hex = $"0x{parseSoldiersSoulsAddr:X8}";
+            
+            // Convert textbox text to int
+            int intValue = Convert.ToInt32(SoldiersSouls_textBox.Text);
+            
+            // Convert & Reverse bytes
+            byte[] intBytes = BitConverter.GetBytes(intValue);
+            Array.Reverse(intBytes);
+            
+            // Write Value in textbox to memory
+            m.WriteBytes(hex, intBytes);
+        }
+
+
+        private void PatternScan_Button_Click(object sender, EventArgs e)
+        {
+            PatternScanSoulsDecrementFunction();
+            PatternScanSoulsLossOnDeath();
+        }
+
+        private void InfiniteHealth_Button_Click(object sender, EventArgs e)
+        {
+            InfiniteHealthHack();
+        }
+
+        private void PatternScan2_Button_Click(object sender, EventArgs e)
+        {
+            //This is to reverse the byte change made by the previous button
+            //Not yet implemented
+            /// Might as well use this for a debug button 
+        }
+        #endregion
+
+
     }
 }
